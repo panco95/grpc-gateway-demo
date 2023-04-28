a7fae5928ae4ff6b2511acd38774e5d7ec39ba7d
Panco <1129443982@qq.com>
2023-04-28 10:25:22 +0800

    fix

--- server/grpc/main.go
+++ server/grpc/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
-	"log"
-	"net"
-	// importing generated stubs
 	"google.golang.org/grpc"
 	gen "grpc_gateway/gen/go/user"
+	"log"
+	"net"
 )
 
 // UserServerImpl will implement the service defined in protocol buffer definitions
@@ -15,18 +14,14 @@ type UserServerImpl struct {
 }
 
 // Login is the implementation of RPC call defined in protocol definitions.
-// This will take HelloRequest message and return HelloReply
 func (g *UserServerImpl) Login(ctx context.Context, request *gen.LoginRequest) (*gen.LoginReply, error) {
 	return &gen.LoginReply{
 		Token: request.Username + request.Password,
 	}, nil
 }
 func main() {
-	// create new gRPC server
 	server := grpc.NewServer()
-	// register the GreeterServerImpl on the gRPC server
 	gen.RegisterUserServer(server, &UserServerImpl{})
-	// start listening on port :8080 for a tcp connection
 	if l, err := net.Listen("tcp", ":8080"); err != nil {
 		log.Fatal("error in listening on port :8080", err)
 	} else {
